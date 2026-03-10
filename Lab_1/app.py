901d7a40a51ad88d08743bb91800c9cef5b05c08
manmartyna1 <mandziukmartyna125@gmail.com>
2026-03-10 19:40:06 +0100

    Dodano model dla użytkownika 420344

--- Lab_1/app.py
+++ Lab_1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, request, jsonify
 from model_0000 import model as model_0000
 from model_123456 import model as model_123456
+from model_420344 import model as model_420344
 
 
 ###########################################
@@ -29,6 +30,15 @@ def model_123456_input():
     input=data["input"]
     #Wykonywanie predykcji
     result=model_123456.run_model_123456(input=input)
+    return jsonify({'result': result}), 200
+
+@app.route('/api/model_420344', methods=['POST'])
+def model_420344_input():
+    # Pobieranie treści zapytania w naszym przypadku array[4]
+    data = request.get_json()
+    input=data["input"]
+    #Wykonywanie predykcji
+    result=model_420344.run_model_420344(input=input)
     return jsonify({'result': result}), 200
 
 

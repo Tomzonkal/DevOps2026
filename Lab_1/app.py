c57f6fe9198ae63b1f9997d6c12d5d70a2a7c658
Kokoszka2004 <kubakukielka@stduent.agh.edu.pl>
2026-03-10 16:09:28 +0100

    nasza zmiana np. - dodano model dla użytkownika 422385

--- Lab_1/app.py
+++ Lab_1/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, request, jsonify
 from model_0000 import model as model_0000
 from model_123456 import model as model_123456
-
+from model_422385 import model as model_422385
 
 ###########################################
 
@@ -20,6 +20,16 @@ def model_00000_input():
     result=model_0000.run_model_0000(input=input)
     return jsonify({'result': result}), 200
 
+
+@app.route('/api/model_422385', methods=['POST'])
+def model_422385_input():
+    # Pobieranie treści zapytania w naszym przypadku array[4]
+    data = request.get_json()
+    input=data["input"]
+    #Wykonywanie predykcji
+    result=model_422385.run_model_422385(input=input)
+    return jsonify({'result': result}), 200
+
 #######################################################################
 
 @app.route('/api/model_123456', methods=['POST'])
@@ -30,6 +40,19 @@ def model_123456_input():
     #Wykonywanie predykcji
     result=model_123456.run_model_123456(input=input)
     return jsonify({'result': result}), 200
+
+#######################################################################
+
+def run_model_422385(input):
+    #Wczytywanie modelu z pliku
+    path= os.path.dirname(__file__)
+    
+    with open(path+"/model.pkl","rb")as f:
+        model= pickle.load(f)
+    #Wykonywanie predykcji    
+    result=model.predict(input)
+    result=float(result[0])
+    return result
 
 
 

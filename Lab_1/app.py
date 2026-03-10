52c448b59c3a87c7fcc0496bd0971560da49312d
kaemiks <kmkm26@onet.pl>
2026-03-10 16:11:48 +0100

    (wiadomosc commita) nasza zmiana np. - dodano model dla użytkownika 422378

--- Lab_1/app.py
+++ Lab_1/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, request, jsonify
 from model_0000 import model as model_0000
 from model_123456 import model as model_123456
-
+from model_422378 import model as model_422378
 
 ###########################################
 
@@ -20,6 +20,15 @@ def model_00000_input():
     result=model_0000.run_model_0000(input=input)
     return jsonify({'result': result}), 200
 
+
+@app.route('/api/model_422378', methods=['POST'])
+def model_422378_input():
+    # Pobieranie treści zapytania w naszym przypadku array[4]
+    data = request.get_json()
+    input=data["input"]
+    #Wykonywanie predykcji
+    result=model_422378.run_model_422378(input=input)
+    return jsonify({'result': result}), 200
 #######################################################################
 
 @app.route('/api/model_123456', methods=['POST'])

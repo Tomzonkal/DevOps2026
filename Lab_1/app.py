5f1607f189f6507932f1fb7a89fe26515bcc16e0
polo910 <mikolaj.polok19@gmail.com>
2026-03-16 19:21:29 +0100

    naprawiony plik

--- Lab_1/app.py
+++ Lab_1/app.py
@@ -12,13 +12,13 @@ from model_422367 import model as model_422367
 app = Flask(__name__)
 
 ##### Ta funkcja powinna być skopiowana do utowrzenia nowego endpointa####################3
-@app.route('/api/model_422367', methods=['POST'])
-def model_422367_input():
+@app.route('/api/model_0000', methods=['POST'])
+def model_00000_input():
     # Pobieranie treści zapytania w naszym przypadku array[4]
     data = request.get_json()
     input=data["input"]
     #Wykonywanie predykcji
-    result=model_422367.run_model_422367(input=input)
+    result=model_0000.run_model_0000(input=input)
     return jsonify({'result': result}), 200
 
 #######################################################################
@@ -30,6 +30,16 @@ def model_123456_input():
     input=data["input"]
     #Wykonywanie predykcji
     result=model_123456.run_model_123456(input=input)
+    return jsonify({'result': result}), 200
+
+##### Ta funkcja powinna być skopiowana do utowrzenia nowego endpointa####################3
+@app.route('/api/model_422367', methods=['POST'])
+def model_422367_input():
+    # Pobieranie treści zapytania w naszym przypadku array[4]
+    data = request.get_json()
+    input=data["input"]
+    #Wykonywanie predykcji
+    result=model_422367.run_model_422367(input=input)
     return jsonify({'result': result}), 200
 
 

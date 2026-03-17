c5b29e496d41367557e63f2a1e08a4240cd1aa1e
grzegorz <gpaszek@student.agh.edu.pl>
2026-03-17 18:03:31 +0100

    zrobiono wersje 2

--- Lab_2/config.py
+++ Lab_2/config.py
@@ -1,2 +1,2 @@
-INDEX = "v1"
-id_list = ["v1"]
+INDEX = "422374"
+id_list = ["422374"]
---
+++ Lab_2/model_422374/model.py
@@ -0,0 +1,13 @@
+import pickle 
+import os 
+
+####### Drugie rozwiązanie ###########
+
+def run_model_v2(input):
+    #Wczytywanie modelu z pliku
+    path= "./model_0000/model.pkl"
+    with open(path,"rb")as f:
+        model= pickle.load(f)    
+        result=model.predict(input)
+        result=float(result[0])
+    return result

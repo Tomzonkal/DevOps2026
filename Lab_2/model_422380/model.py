a102bb738070e136d3c92dd15dd3b1248813a51d
Alan2113 <alan.ozga3@gmail.com>
2026-03-17 17:52:31 +0100

    zrobiono wersje 3

---
+++ Lab_2/model_422380/model.py
@@ -0,0 +1,10 @@
+import pickle
+import os
+
+def run_model_422380_v3(input):
+    path= os.path.dirname(__file__)
+    with open(path+"/model.pkl","rb")as f:
+        model= pickle.load(f)
+    result=model.predict(input)
+    result=float(result[0])
+    return result

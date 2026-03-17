08717ab269118f52317cdb80924f87d2161d0912
Alan2113 <alan.ozga3@gmail.com>
2026-03-17 17:49:01 +0100

    zrobiona wersje 1

--- Lab_2/app.py
+++ Lab_2/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, jsonify
-from model_422380 import model as model_422380
+from model_422380 import model
 
 app = Flask(__name__)
 
-@app.route('/api/model_422380', methods=['POST'])
+@app.route('/api/model_v1', methods=['POST'])
 def model_v1_input():
     data = request.get_json()
     input = data["input"]
-    result_v1 = model.run_model_v1(input)
+    result_v1 = model.run_model_422380_v1(input)
     return jsonify({'result': result_v1}), 200
 
 if __name__ == '__main__':
--- Lab_2/config.py
+++ Lab_2/config.py
@@ -1,2 +1,2 @@
 INDEX = "v1"
-id_list = ["v1", "422380"]
+id_list = ["v1"]

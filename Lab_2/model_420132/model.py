f0ecfb562310682d1f4aa26cb4b56dbdd737e64d
ulkae <urszula.pyka2709@gmail.com>
2026-03-17 15:40:25 +0100

    zrobiono wersje 1

---
+++ Lab_2/model_420132/model.py
@@ -0,0 +1,15 @@
+import pickle 
+import os 
+
+###### Pierwsze rozwiązanie ###########
+
+def run_model_420132_v1(input):
+    #Wczytywanie modelu z pliku
+    path= os.path.dirname(__file__)
+    
+    with open(path+"/model.pkl","rb")as f:
+        model= pickle.load(f)
+    #Wykonywanie predykcji    
+    result=model.predict(input)
+    result=float(result[0])
+    return result

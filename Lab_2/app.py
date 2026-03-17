72bd929a212c09abb683d48624f3e52e3a20e0fb
olap732 <aleksandraprzewoznik@agh.edu.pl>
2026-03-17 16:29:03 +0100

    zrobiono wersje 3

--- Lab_2/app.py
+++ Lab_2/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, jsonify
-from model_0000 import model
+from model_419827 import model
 
 app = Flask(__name__)
 
-@app.route('/api/model_v1', methods=['POST'])
-def model_v1_input():
+@app.route('/api/model_v3', methods=['POST'])
+def model_v3_input():
     data = request.get_json()
     input = data["input"]
-    result_v1 = model.run_model_v1(input)
+    result_v3 = model.run_model_v3(input)
     return jsonify({'result': result_v1}), 200
 
 if __name__ == '__main__':

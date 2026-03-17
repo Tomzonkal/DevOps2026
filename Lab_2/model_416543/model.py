dee9fd2e2f76562a709bdcb48bb9f58f601a51e5
PanWasacz <michalekmroz@gmail.com>
2026-03-17 15:43:00 +0100

    usunieto odstepy v3

--- Lab_2/model_416543/model.py
+++ Lab_2/model_416543/model.py
@@ -1,17 +1,12 @@
-import pickle 
-import os 
-
-
-####### Trzecie rozwiązanie ###########
-
-def run_model_v3(input):
+import pickle
+import os
+def run_model_416543_v3(input):
     #Wczytywanie modelu z pliku
     path= os.path.dirname(__file__)
-    
     f=open(path+"/model.pkl","rb")
     model= pickle.load(f)
     f.close()
-    #Wykonywanie predykcji    
+    #Wykonywanie predykcji
     result=model.predict(input)
     result=float(result[0])
     return result

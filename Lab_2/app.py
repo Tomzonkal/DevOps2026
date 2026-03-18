2c0e412ec2e0ea2ba893faf2abd85d78697acf9a
manmartyna1 <mandziukmartyna125@gmail.com>
2026-03-19 00:11:29 +0100

    Poprawiono lab 2 pod testy

--- Lab_2/app.py
+++ Lab_2/app.py
@@ -4,28 +4,16 @@ from model_420344 import model as model_420344
 
 app = Flask(__name__)
 
-@app.route('/api/model_v1', methods=['POST'])
-def model_v1_input():
+@app.route('/api/model_420344', methods=['POST'])
+def model_420344_input():
     data = request.get_json()
     input = data["input"]
+
     result_v1 = model_420344.run_model_v1(input)
-    return jsonify({'result': result_v1}), 200
-
-
-@app.route('/api/model_v2', methods=['POST'])
-def model_v2_input():
-    data = request.get_json()
-    input = data["input"]
     result_v2 = model_420344.run_model_v2(input)
-    return jsonify({'result': result_v2}), 200
-
-
-@app.route('/api/model_v3', methods=['POST'])
-def model_v3_input():
-    data = request.get_json()
-    input = data["input"]
     result_v3 = model_420344.run_model_v3(input)
-    return jsonify({'result': result_v3}), 200
+
+    return jsonify({'result': [result_v1, result_v2, result_v3]}), 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=5000)

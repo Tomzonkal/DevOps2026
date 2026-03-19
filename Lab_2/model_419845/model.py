19df1e35e9e846fd0c978de453f788de19553af5
mextryy <atryndoch@student.agh.edu.pl>
2026-03-19 10:30:28 +0100

    zrobiono wersje 1

--- Lab_2/model_419845/model.py
+++ Lab_2/model_419845/model.py
@@ -3,7 +3,7 @@ import os
 
 ###### Pierwsze rozwiązanie ###########
 
-def run_model_v1(input):
+def run_model_419845(input):
     #Wczytywanie modelu z pliku
     path= os.path.dirname(__file__)
     

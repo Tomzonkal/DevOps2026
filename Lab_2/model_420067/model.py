e07985a952aa77604ff378bc8c5dd88417ce9f20
magdalena-vx <kubatmagdalena33@gmail.com>
2026-03-23 21:36:10 +0100

    fix formatting

--- Lab_2/model_420067/model.py
+++ Lab_2/model_420067/model.py
@@ -1,20 +1,26 @@
-import pickle 
-import os 
+import os
+import pickle
+
+
 def run_model_v1(input):
-    path= os.path.dirname(__file__)
-    
-    with open(path+"/model.pkl","rb")as f:
-        model= pickle.load(f)
-    result=model.predict(input)
-    result=float(result[0])
+    path = os.path.dirname(__file__)
+
+    with open(path + "/model.pkl", "rb") as f:
+        model = pickle.load(f)
+    result = model.predict(input)
+    result = float(result[0])
     return result
+
+
 def run_model_v2(input):
-    path= "./model_0000/model.pkl"
-    with open(path,"rb")as f:
-        model= pickle.load(f)    
-        result=model.predict(input)
-        result=float(result[0])
+    path = "./model_0000/model.pkl"
+    with open(path, "rb") as f:
+        model = pickle.load(f)
+        result = model.predict(input)
+        result = float(result[0])
     return result
+
+
 def run_model_v3(input):
     path = os.path.dirname(__file__)
     f = open(path + "/model.pkl", "rb")

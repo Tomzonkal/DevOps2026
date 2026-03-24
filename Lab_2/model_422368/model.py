c46d85eeca027ef616a7ece947c3dc004eb959f8
mat-nya <emha4913@gmail.com>
2026-03-24 18:01:24 +0100

    Poprawa v1

--- Lab_2/model_422368/model.py
+++ Lab_2/model_422368/model.py
@@ -3,9 +3,8 @@ import os
 def run_model_v1(input):
     #Wczytywanie modelu z pliku
     path= os.path.dirname(__file__)
-    f=open(path+"/model.pkl","rb")
-    model= pickle.load(f)
-    f.close()
+    with open(path+"/model.pkl","rb")as f:
+        model= pickle.load(f)
     #Wykonywanie predykcji    
     result=model.predict(input)
     result=float(result[0])

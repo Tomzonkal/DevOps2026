81e0f10936c1bb1b9debece50043f8b47cec0504
szymon <szymonros195@gmail.com>
2026-03-24 13:34:40 +0100

    wersja 2

--- Lab_2/app.py
+++ Lab_2/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from model_0000 import model
-
+from model_422973 import model as model_422973
 app = Flask(__name__)
 
 @app.route('/api/model_v1', methods=['POST'])
@@ -8,6 +8,13 @@ def model_v1_input():
     data = request.get_json()
     input = data["input"]
     result_v1 = model.run_model_v1(input)
+    return jsonify({'result': result_v1}), 200
+
+@app.route('/api/model_422973', methods=['POST'])
+def model_422973_input():
+    data = request.get_json()
+    input = data["input"]
+    result_v1 = model_422973.run_model_422973(input)
     return jsonify({'result': result_v1}), 200
 
 if __name__ == '__main__':

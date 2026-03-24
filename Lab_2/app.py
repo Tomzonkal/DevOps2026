b2488b69b947158494a3c6a45bb8c8f9bfc54ec7
PanWasacz <michalekmroz@gmail.com>
2026-03-24 11:00:09 +0100

    sformatowano kod black i isort

--- Lab_2/app.py
+++ Lab_2/app.py
@@ -1,24 +1,33 @@
-from flask import Flask, request, jsonify
-from model_416543 import model as model_416543
+from flask import Flask, jsonify, request
 from model_0000 import model
+from model_416543 import model as model_416543
+
 app = Flask(__name__)
-@app.route('/api/model_v1', methods=['POST'])
+
+
+@app.route("/api/model_v1", methods=["POST"])
 def model_v1_input():
     data = request.get_json()
     input = data["input"]
     result_v1 = model_416543.run_model_416543_v1(input=input)
-    return jsonify({'result': result_v1}), 200
-@app.route('/api/model_v2', methods=['POST'])
+    return jsonify({"result": result_v1}), 200
+
+
+@app.route("/api/model_v2", methods=["POST"])
 def model_v2_input():
     data = request.get_json()
     input = data["input"]
     result_v2 = model_416543.run_model_416543_v2(input=input)
-    return jsonify({'result': result_v2}), 200
-@app.route('/api/model_v3', methods=['POST'])
+    return jsonify({"result": result_v2}), 200
+
+
+@app.route("/api/model_v3", methods=["POST"])
 def model_v3_input():
     data = request.get_json()
     input = data["input"]
     result_v3 = model_416543.run_model_416543_v3(input=input)
-    return jsonify({'result': result_v3}), 200
-if __name__ == '__main__':
+    return jsonify({"result": result_v3}), 200
+
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)

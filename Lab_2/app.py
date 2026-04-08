f802841cd959afe1c82c950e6d9b5b807dd2b5ea
stysiak <jkozub@student.agh.edu.pl>
2026-04-08 22:27:26 +0200

    zrobiono wersje 3

--- Lab_2/app.py
+++ Lab_2/app.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, jsonify
-from model_0000 import model
-from model_401967 import model
+from model_401967 import model as model
 
 app = Flask(__name__)
 
-@app.route('/api/model_v3', methods=['POST'])
-def model_v3_input():
+@app.route('/api/model_401967', methods=['POST'])
+def model_input():
     data = request.get_json()
     input = data["input"]
     result_v3 = model.run_model_v3(input)

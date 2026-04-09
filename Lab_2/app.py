eea61ebb6b89f922999734b218e9cfeecfaaf031
Hubert Stach <hubertstach@student.agh.edu.pl>
2026-04-09 13:13:06 +0200

    poprawiona v2

--- Lab_2/app.py
+++ Lab_2/app.py
@@ -4,12 +4,18 @@ from model_420033 import model as model_420033_v2
 
 app = Flask(__name__)
 
-@app.route('/api/model_v2', methods=['POST'])
+@app.route('/api/model_v1', methods=['POST'])
 def model_v1_input():
     data = request.get_json()
     input = data["input"]
-    result_v1 = model_420033_v2.run_model_v2(input)
+    result_v1 = model.run_model_v1(input)
     return jsonify({'result': result_v1}), 200
+@app.route('/api/model_420033_v2', methods=['POST'])
+def model_420033_v2_input():
+    data = request.get_json()
+    input = data["input"]
+    result = model_420033_v2.run_model_v2(input)
+    return jsonify({'result': result}), 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=5000)

841496add32c3ec08091eccb2a531c646551db70
kaemiks <kmkm26@onet.pl>
2026-04-10 21:56:05 +0200

    Naprawa formatowania wszystkich plików (black/isort)

--- Lab_2/model_422378/model.py
+++ Lab_2/model_422378/model.py
@@ -1,27 +1,30 @@
-import pickle 
-import os 
+import os
+import pickle
+
 
 def run_model_v1(input):
-    path= os.path.dirname(__file__)
-    with open(path+"/model.pkl","rb")as f:
-        model= pickle.load(f)
-    result=model.predict(input)
-    result=float(result[0])
+    path = os.path.dirname(__file__)
+    with open(path + "/model.pkl", "rb") as f:
+        model = pickle.load(f)
+    result = model.predict(input)
+    result = float(result[0])
     return result
+
 
 def run_model_v2(input):
-    path= "./model_422378/model.pkl"
-    with open(path,"rb")as f:
-        model= pickle.load(f)    
-        result=model.predict(input)
-        result=float(result[0])
+    path = "./model_422378/model.pkl"
+    with open(path, "rb") as f:
+        model = pickle.load(f)
+        result = model.predict(input)
+        result = float(result[0])
     return result
 
+
 def run_model_v3(input):
-    path= os.path.dirname(__file__)
-    f=open(path+"/model.pkl","rb")
-    model= pickle.load(f)
+    path = os.path.dirname(__file__)
+    f = open(path + "/model.pkl", "rb")
+    model = pickle.load(f)
     f.close()
-    result=model.predict(input)
-    result=float(result[0])
+    result = model.predict(input)
+    result = float(result[0])
     return result

a20e19ca7c41f6d33b8af3ce43a383ef32b7ccbc
szymon <szymonros195@gmail.com>
2026-04-10 19:44:16 +0200

    poprawki v1

--- Lab_2/model_422973/model.py
+++ Lab_2/model_422973/model.py
@@ -13,13 +13,3 @@ def run_model_v1(input):
     result=model.predict(input)
     result=float(result[0])
     return result
-###### Drugie rozwiązanie ###########
-
-def run_model_422973_v1(input):
-    #Wczytywanie modelu z pliku
-    path= "./model_0000/model.pkl"
-    with open(path,"rb")as f:
-        model= pickle.load(f)    
-        result=model.predict(input)
-        result=float(result[0])
-    return result

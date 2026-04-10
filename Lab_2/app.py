2ad3e315dc44b3661dcd27df9cb0fb2334afcd34
kaemiks <kmkm26@onet.pl>
2026-04-10 21:37:10 +0200

    Formatowanie kodu pod testy serwera

--- Lab_2/app.py
+++ Lab_2/app.py
@@ -1,28 +1,32 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 from model_422378 import model
 
 app = Flask(__name__)
 
-@app.route('/api/model_v1', methods=['POST'])
+
+@app.route("/api/model_v1", methods=["POST"])
 def model_v1_input():
     data = request.get_json()
     input = data["input"]
     result_v1 = model.run_model_v1(input)
-    return jsonify({'result': result_v1}), 200
+    return jsonify({"result": result_v1}), 200
 
-@app.route('/api/model_v2', methods=['POST'])
+
+@app.route("/api/model_v2", methods=["POST"])
 def model_v2_input():
     data = request.get_json()
     input = data["input"]
     result_v2 = model.run_model_v2(input)
-    return jsonify({'result': result_v2}), 200
+    return jsonify({"result": result_v2}), 200
 
-@app.route('/api/model_v3', methods=['POST'])
+
+@app.route("/api/model_v3", methods=["POST"])
 def model_v3_input():
     data = request.get_json()
     input = data["input"]
     result_v3 = model.run_model_v3(input)
-    return jsonify({'result': result_v3}), 200
+    return jsonify({"result": result_v3}), 200
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)

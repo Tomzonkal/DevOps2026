64bfddadc69465f81391a2f5a8f8b24ec9b11313
Magda Put <mput@student.agh.edu.pl>
2026-04-11 14:38:20 +0200

    zrobiono wersje v1 clean

--- Lab_2/app.py
+++ Lab_2/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from model_0000 import model
+from model_422364 import model
 
 app = Flask(__name__)
 
@@ -7,7 +7,9 @@ app = Flask(__name__)
 def model_v1_input():
     data = request.get_json()
     input = data["input"]
+
     result_v1 = model.run_model_v1(input)
+
     return jsonify({'result': result_v1}), 200
 
 if __name__ == '__main__':

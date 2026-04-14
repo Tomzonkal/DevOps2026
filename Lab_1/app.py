a25ea3e2ba0d998ef99e954002a1c6c7f4f3a04c
wiktoriarhlk <rychlakwiktoria@gmail.com>
2026-04-14 20:19:00 +0200

    lab5: black/isort fix + usun .env + fix gitignore

--- Lab_1/app.py
+++ Lab_1/app.py
@@ -1,5 +1,4 @@
-
-# W tej sekcji dodajemy nasz nowy model 
+# W tej sekcji dodajemy nasz nowy model
 from flask import Flask, jsonify, request
 from model_0000 import model as model_0000
 from model_123456 import model as model_123456
@@ -9,30 +8,30 @@ from model_123456 import model as model_123456
 
 app = Flask(__name__)
 
+
 ##### Ta funkcja powinna być skopiowana do utowrzenia nowego endpointa####################3
-@app.route('/api/model_0000', methods=['POST'])
+@app.route("/api/model_0000", methods=["POST"])
 def model_00000_input():
     # Pobieranie treści zapytania w naszym przypadku array[4]
     data = request.get_json()
-    input=data["input"]
-    #Wykonywanie predykcji
-    result=model_0000.run_model_0000(input=input)
-    return jsonify({'result': result}), 200
+    input = data["input"]
+    # Wykonywanie predykcji
+    result = model_0000.run_model_0000(input=input)
+    return jsonify({"result": result}), 200
+
 
 #######################################################################
 
-@app.route('/api/model_123456', methods=['POST'])
+
+@app.route("/api/model_123456", methods=["POST"])
 def model_123456_input():
     # Pobieranie treści zapytania w naszym przypadku array[4]
     data = request.get_json()
-    input=data["input"]
-    #Wykonywanie predykcji
-    result=model_123456.run_model_123456(input=input)
-    return jsonify({'result': result}), 200
+    input = data["input"]
+    # Wykonywanie predykcji
+    result = model_123456.run_model_123456(input=input)
+    return jsonify({"result": result}), 200
 
 
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True,port=5000)
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=5000)

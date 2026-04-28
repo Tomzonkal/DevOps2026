e6da4f94ddbbdac3dfc68af8ab2828ca613fb464
wiktoriarhlk <rychlakwiktoria@gmail.com>
2026-04-28 22:03:54 +0200

    formatowanie Lab_1/2/3

--- Lab_2/app.py
+++ Lab_2/app.py
@@ -3,12 +3,14 @@ from model_0000 import model
 
 app = Flask(__name__)
 
-@app.route('/api/model_v1', methods=['POST'])
+
+@app.route("/api/model_v1", methods=["POST"])
 def model_v1_input():
     data = request.get_json()
     input = data["input"]
     result_v1 = model.run_model_v1(input)
-    return jsonify({'result': result_v1}), 200
+    return jsonify({"result": result_v1}), 200
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)

2070e611c5c4ab8f48fbfb36d0fe6ff69a2addaa
magdalena-vx <kubatmagdalena33@gmail.com>
2026-06-17 15:25:31 +0200

    projekt 420067: test4

--- Projekt/app/app.py
+++ Projekt/app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "message": "DevOps 2026 test3",
+        "message": "DevOps 2026 test4",
         "status": "running"
     })
 

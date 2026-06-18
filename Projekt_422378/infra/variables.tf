7b537554e0eb6b31c73393dc84da69652d246165
kaemiks <kmkm26@onet.pl>
2026-06-18 23:14:39 +0200

    Uruchomienie infrastruktury CI/CD 4.0

---
+++ Projekt_422378/infra/variables.tf
@@ -0,0 +1,4 @@
+variable "resource_group_name" { default = "rg-projekt-422378" }
+variable "location" { default = "polandcentral" }
+variable "acr_name" { default = "acr422378devops" } 
+variable "aks_name" { default = "aks-projekt-422378" }

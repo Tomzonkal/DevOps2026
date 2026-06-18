7b581160af5c6c097167fce2a20d7dc143ff5759
PanWasacz <michalekmroz@gmail.com>
2026-06-18 02:58:01 +0200

    projekt: dodano pliki aplikacji i infrastruktury

---
+++ Projekt/416543/infra/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type    = string
+  default = "rg-projekt416543"
+}
+
+variable "location" {
+  type    = string
+  default = "polandcentral"
+}
+
+variable "acr_name" {
+  type    = string
+  default = "acrprojekt416543"
+}
+
+variable "aks_name" {
+  type    = string
+  default = "aks-projekt416543"
+}

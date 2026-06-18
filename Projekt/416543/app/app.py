1fb27433b84174ce452371a7d040479ac18d6536
PanWasacz <michalekmroz@gmail.com>
2026-06-18 16:22:57 +0200

    drugi test

--- Projekt/416543/app/app.py
+++ Projekt/416543/app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify({"message": "Hello from DevOps 2026! NA sigmie", "student": "416543"})
+    return jsonify({"message": "Hello from DevOps 2026! NA sigmie! COMMIT 3", "student": "416543"})
 
 
 @app.route("/health")
